5c7ba44636f3095b4e6514460e360a79e2fd4b4f
Stefan <stefansternberg@gmail.com>
2020-09-14 15:02:37 -0700

    Adding in my Edge-centered polyhedron

---
+++ CSharp/Edge.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace polyhedraV3{
+	/**
+	 *
+	 * @author Stefan
+	 */
+	public class Edge {
+		/**
+		* Front and back are vertices.
+		* Left and right are faces;
+		* The following should always be true
+		* next.back == this.front 
+		* prev.front == this.back
+		* next.left == this.left == prev.left
+		* this.right == reverse.left;
+		*/
+		public int f, b, l, r;
+
+		public int F() {
+			return f;
+		}
+		public int B() {
+			return b;
+		}
+		public int L() {
+			return l;
+		}
+		public int R() {
+			return r;
+		}
+		public int N() {
+			return n;
+		}
+		public int P() {
+			return p;
+		}
+		public int Rev() {
+			return rev;
+		}
+		public int Cw() {
+			return cw;
+		}
+		public int Ccw() {
+			return ccw;
+		}
+
+		//u is about the front
+		//d is about the back
+		public int n, p, rev, cw, ccw;
+		//    Edge next;
+		//    Edge prev;
+		//    Edge reverse;
+
+		internal int data_val;
+
+		public int data(){
+		   return data_val;
+		}
+
+		public Edge next<F,E,V>( EdgeHedron<F,E,V> parent ){
+			return parent.getEdge(n);
+		}
+		public Edge prev<F,E,V>( EdgeHedron<F,E,V> parent ){
+			return parent.getEdge(p);
+		}
+		public Edge reverse<F,E,V>( EdgeHedron<F,E,V> parent ){
+			return parent.getEdge(rev);
+		}
+		public Edge clockwise<F,E,V>( EdgeHedron<F,E,V> parent ){
+			return parent.getEdge(cw);
+		}
+		public Edge cClockwise<F,E,V>( EdgeHedron<F,E,V> parent ){
+			return parent.getEdge(ccw);
+		}
+		public E getData <F,E,V>( EdgeHedron<F,E,V> parent ){
+			return parent.getE(data_val);
+		}
+		
+		
+		public Edge invert<F,E,V>( EdgeHedron<F,E,V> parent ){
+			Edge i = new Edge();
+			i.l = this.b;
+			i.b = this.r;
+			i.r= this.f;
+			i.f= this.l;
+			
+			i.n = this.prev(parent).rev;
+			i.p = this.reverse(parent).n;
+			i.rev = this.rev;
+			i.cw = this.p;
+			i.ccw = this.n;
+			
+			return i;
+		}
+	}
+}
+
---
+++ CSharp/EdgeHedron.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace polyhedraV3{
+	public class EdgeHedron <F,E,V>
+	{
+		private Edge[] edges;
+		private E[] edgeData;
+		private F[] faceData;
+		private V[] vertexData;
+		
+		public E getE(int i){
+			return edgeData[i];
+		}
+		public F getF(int i){
+			return faceData[i];
+		}
+		public V getV(int i){
+			return vertexData[i];
+		}
+		public Edge getEdge(int i){
+			return edges[i];
+		}
+		
+		public Edge[] Edges{
+			get{
+				return edges;
+			}
+		}
+		public F[] FaceData{
+			get{
+				return faceData;
+			}
+		}
+		public E[] EdgeData{
+			get{
+				return edgeData;
+			}
+		}
+		public V[] VertexData{
+			get{
+				return vertexData;
+			}
+		}
+		
+		
+		public EdgeHedron(Edge[] es, F[] fds, E[] eds, V[] vds){
+			this.edges = es;
+			this.faceData = fds;
+			this.edgeData = eds;
+			this.vertexData = vds;
+		}
+		public EdgeHedron(List<Edge> es, List<F> fds, List<E> eds,  List<V> vds){
+			edges = es.ToArray();
+			faceData = fds.ToArray();
+			vertexData = vds.ToArray();
+			edgeData = eds.ToArray();
+		}
+		
+		public EdgeHedron<V,E,F> Invert(){
+			return new EdgeHedron<V,E,F>(
+					this.edges
+						.Select(e=>e.invert(this))
+						.ToArray(),
+					this.vertexData,
+					this.edgeData,
+					this.faceData
+				);
+		}
+		
+		public static void matchPartials(List<Edge> mypes){
+			List<Edge> copy = new List<Edge>(mypes.Count);
+			copy.AddRange(mypes);
+			matchPartials(copy,mypes);
+		}
+		public static void matchPartials(List<Edge> mypes, List<Edge> original){
+			if(mypes.Count==0)
+				return;
+			
+			while(mypes.Count!=0){
+				
+				bool foundMatch = false;
+				
+				Edge e1 = mypes[0];
+				for(int i=1; i<mypes.Count; i++){
+					Edge e2 = mypes[i];
+					if(e1.f == e2.b && e1.b == e2.f){
+						e1.r = e2.l;
+						e2.r = e1.l;
+						e1.rev = original.IndexOf(e2);
+						e2.rev = original.IndexOf(e1);
+
+						mypes.RemoveAt(i);
+						mypes.RemoveAt(0);
+						
+						foundMatch = true;
+						break;
+					}
+				}
+				
+				if(!foundMatch)
+					throw new ArgumentException("Missing match. Not all given edges have a match");
+			}
+		}
+	}
+}
