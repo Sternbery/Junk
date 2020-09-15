1693325bf765f8dce1fe9a2e4156e31cea61f684
Stefan <stefansternberg@gmail.com>
2020-09-15 16:30:03 -0700

    added a Face-centric representation of a polyhedron Facehedron and Edgehedron will complement each other

---
+++ CSharp/Face.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace polyhedraV3{
+	public class Face{
+		
+		private int data;
+		private Tuple<int,int>[] sides = new Tuple<int,int>[0];
+		
+		public Face(){}
+		public Face(int d, Tuple<int,int>[] mysides){
+			this.data = d;
+			this.sides = mysides;
+		}
+		
+		public void Add(int v, int e){
+			this.Add(Tuple.Create(v,e));
+		}
+		
+		public void Add(Tuple<int, int> ev){
+			Tuple<int,int>[] newsides = new Tuple<int, int>[sides.Length+1];
+			sides.CopyTo(newsides,0);
+			newsides[sides.Length] = ev;
+			sides = newsides;
+			
+		}
+		
+		public Tuple<int,int> this[int index]{
+			get{
+				return sides[index];
+			}
+		}
+		public int Count(){
+			return sides.Length;
+		}
+		public List<Tuple<int,int>> ToList{
+			get{
+				return sides.ToList();
+			}
+		}
+		
+		public int Data{
+			get{
+				return this.data;
+			}
+		}
+		public List<int> Vertices {
+			get{
+				return sides.ToList().Select(s=>s.Item1).ToList();
+			}
+		}
+		public List<int> Edges {
+			get {
+				return sides.ToList().Select(s=>s.Item2).ToList();
+			}
+		}
+		
+		// public List<Tuple<int,int,int>> Triangles{
+			// get{
+				// List<Tuple<int,int,int>> tris = new List<Tuple<int,int,int>>();
+				// for(int v =1; v+1<this.Count();v++){
+					// tris.Add(Tuple.Create(
+						// this.Vertices[0],
+						// this.Vertices[v],
+						// this.Vertices[v+1]
+						// ));
+					
+				// }
+				// return tris;
+			// }
+		// }
+		
+		public F GetData<F,E,V>(FaceHedron<F,E,V> parent){
+			return parent.FaceData[this.data];
+		}
+		public V GetVertex<F,E,V>(int index, FaceHedron<F,E,V> parent){
+			return parent.VertexData[sides[index].Item1];
+		}
+		public List<V> GetVertices<F,E,V>(FaceHedron<F,E,V> parent){
+			return sides.Select(t=>parent.VertexData[t.Item1]).ToList();
+		}
+	}
+}
+
---
+++ CSharp/FaceHedron.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace polyhedraV3{
+	public class FaceHedron <F,E,V>
+	{
+		private Face[] faces;
+		private F[] faceData;
+		private E[] edgeData;
+		private V[] vertexData;
+	
+		public FaceHedron(Face[] fs, F[] fds, E[] eds, V[] vds){
+			faces = fs;
+			faceData = fds;
+			edgeData = eds;
+			vertexData=vds;
+		}
+		public FaceHedron(List<Face> fs, List<F> fds, List<E> eds, List<V> vds){
+			faces = fs.ToArray();
+			faceData = fds.ToArray();
+			edgeData = eds.ToArray();
+			vertexData=vds.ToArray();
+		}
+		
+		public Face[] Faces{
+			get{
+				return faces;
+			}
+		}
+		public F[] FaceData{
+			get{
+				return faceData;
+			}
+		}
+		public E[] EdgeData{
+			get{
+				return edgeData;
+			}
+		}
+		public V[] VertexData{
+			get{
+				return vertexData;
+			}
+		}
+		
+		public FaceHedron<O,E,V> MapFaces<O>(Func<Face,F,O> f2o){
+			return new FaceHedron<O,E,V>(
+				this.faces,
+				this.faces
+					.Select(f=>f2o(f,f.GetData(this)))
+					.ToArray(),
+				this.edgeData,
+				this.vertexData
+			);
+		}
+		public FaceHedron<O,E,V> MapFaces<O>(Func<F,O> f2o){
+			return new FaceHedron<O,E,V>(
+				this.faces,
+				this.faceData.Select(f2o).ToArray(),
+				this.edgeData,
+				this.vertexData
+			);
+		}
+		public FaceHedron<F,O,V> MapEdges<O>(Func<E,O> e2o){
+			return new FaceHedron<F,O,V>(
+				this.faces,
+				this.faceData,
+				this.edgeData.Select(e2o).ToArray(),
+				this.vertexData
+			);
+		}
+		
+		public FaceHedron<F,E,O> MapVerts<O>(Func<V,O> v2o){
+			return new FaceHedron<F,E,O>(
+				this.faces,
+				this.faceData,
+				this.edgeData,
+				this.vertexData.Select(v2o).ToArray()
+			);
+		}
+		
+
+		
+		public int NumFaces{
+			get{
+				return faces.Length;
+			}
+		}
+
+	}
+}
